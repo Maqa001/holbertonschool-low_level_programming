94b7f5e9254c7c05f8db959a7deffeff72686636
Maqa001 <mma.malikov@mail.ru>
2024-02-20 16:11:08 +0400

    l

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 
+/*
+ * File: 5-printf.c
+ * Auth: Maqa001
+ */
 int main(void)
+/**
+ * main - Prints "with proper grammar, but the outcome is
+ *                a piece of art," followed by a new line.
+ *
+ * Return: Always 0.
+ */
+
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 
