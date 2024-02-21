d948a6579f05be0ea8947a14a09b77b64448cd9c
Maqa001 <mm.malikov@mail.ru>
2024-02-21 07:53:37 +0000

    m

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 
-int main(void)
 
 /**
  * main - Prints "with proper grammar, but the outcome is
@@ -8,7 +7,7 @@ int main(void)
  *
  * Return: Always 0.
  */
-
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 
