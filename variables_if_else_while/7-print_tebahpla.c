a33c547dd3d3f3dc4a5518c4d0adbd99b3654a6d
Maqa001 <mm.malikov@mail.ru>
2024-02-21 09:47:31 +0000

    l

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+  * main - Prints the numbers since 0 to 9
+  *
+  * Return: Always (Success)
+  */
+
+int main(void)
+{
+	char n;
+
+	for (c = 'z', c >= 'a', c++)
+	{
+		putchar(c)
+	}
+	putchar('\n');
+	return (0);
+}
