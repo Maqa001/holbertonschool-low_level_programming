1b2f26ab188f3fedaaa59acd3abda59594f94d2c
Maqa001 <mm.malikov@mail.ru>
2024-02-21 10:37:46 +0000

    k

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -9,16 +9,19 @@
 int main(void)
 {
 	int n;
+
 	for (n = '0'; n <= '9'; n++)
 	{
 		putchar(n);
+
 		if (n !=  '9')
 		{
 			putchar(',');
 			putchar(' ');
 		}
 	}
-	putchar('\n');
-	return (0);
 
+	putchar('\n');
+
+	return (0);
 }
