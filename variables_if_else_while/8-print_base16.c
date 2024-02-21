97660849ca839e4f419a46f4e23793b84f104ed4
Maqa001 <mm.malikov@mail.ru>
2024-02-21 09:55:07 +0000

    l

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+  * main - Prints the numbers since 0 to 9
+  *
+  * Return: Always (Success)
+  */
+
+int main(void)
+{
+	char c;
+
+	for(c = '0'; c <= '9'; c++)
+	{
+		putchar(c);
+	}
+	for(c = 'a'; c <= 'f'; c++)
+	{
+		putchar(c);
+	}
+	putchar('/n');
+	return (0);
+
+}
