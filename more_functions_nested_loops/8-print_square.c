3fe091ba0ccabfd2b526f00028480c1a4a8b6822
Maqa001 <mma.malikov@mail.ru>
2024-02-28 13:17:43 +0000

    l

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -1,24 +1,24 @@
 #include "main.h"
 
 /**
-  * print_square - funtion
-  * @size: argument
-  * Return: good
-  */
+* print_square - funtion
+* @size: argument
+* Return: good
+*/
 
 void print_square(int size)
 {
-        int i, j;
+	int i, j;
 
-        if (size <= 0)
-                _putchar('\n');
+	if (size <= 0)
+		_putchar('\n');
 
-        for (i = 0; i < size; i++)
-        {
-                for (j = 0; j < size; j++)
-                {
-                        _putchar('#');
-                }
-                _putchar('\n');
-        }
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
 }
