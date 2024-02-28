3cd20f821d86be03dfa4ea48dbf972110bfeec40
Maqa001 <101941356+Maqa001@users.noreply.github.com>
2024-02-28 17:02:52 +0400

    Update 8-print_square.c

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+\**
+  * print_square - funtion 
+  * @size: argument
+  * Return: good
+  */ 
+
+
+void print_square(int size)
+{
+        int i, j;
+
+        if (size <= 0)
+                _putchat('\n');
+
+        for (i = 0; i < size; i++)
+        {
+                for (j = 0; j < i; j++)
+                {
+                        _putchar('#');
+                }
+                _putchar('\n');
+        }
+
+}
