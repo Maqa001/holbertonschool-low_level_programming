73f806d9dd5bfbdd5ee7264b21306fbcdc9f761c
Maqa001 <mma.malikov@mail.ru>
2024-02-28 10:18:09 +0000

    k

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* print_line - fct jhk
+* _putchar - is function
+* @n: number of times
+* Return: retn void
+*/
+void print_line(int n)
+{
+	int i;
+	for (i = 0; i < n; i++)
+	{
+		_putcahr('_');
+	}
+	_putcahr('\n');
+}
