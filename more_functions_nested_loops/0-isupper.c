1127aac6c7ab1fe4ea70db1267f3e12a26e196e0
Maqa001 <mma.malikov@mail.ru>
2024-02-28 08:52:04 +0000

    l

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,15 +1,17 @@
 #include "main.h"
+
 /**
- * main - is correct
+ * _isupper - uppercase letters
+ * @c: character to check
  *
- * Result is correct 
+ * Return: 0 or 1
  */
-
-
 int _isupper(int c)
+
 {
 	if (c >= 'A' && c <= 'Z')
 		return (1);
 	else
 		return (0);
 }
+
