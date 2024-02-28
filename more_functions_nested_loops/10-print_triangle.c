09dea6335a6412a29be4de179e66b28e67bb5a19
Maqa001 <mma.malikov@mail.ru>
2024-02-28 13:46:51 +0000

    l

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -1,26 +1,34 @@
 #include "main.h"
 
 /**
-*
-*
-*
-*/
+ * print_triangle - entry point
+ * owned by Bwave/Bright Daniel
+ * Description: Prints diagonals
+ *@size: size of the triangle
+ * Return: void
+ */
 
 void print_triangle(int size)
 {
-	int i, j, z;
+	int row, hashes, spaces;
 
 	if (size <= 0)
-		_putchar('\n');
-	for (i = 1; i < size; i++)
 	{
-		for (j = size - i ; j--)
+		_putchar('\n');
+	}
+	else
+	{
+		for (row = 1; row <= size; row++)
 		{
-			_putchar(' ');
-			for (z = 1; z < size; z++)
+			for (spaces = size - row; spaces >= 1; spaces--)
+			{
+				_putchar(' ');
+			}
+			for (hashes = 1; hashes <= row; hashes++)
+			{
 				_putchar('#');
-				_putchar('\n');
+			}
+			_putchar('\n');
 		}
 	}
-
 }
