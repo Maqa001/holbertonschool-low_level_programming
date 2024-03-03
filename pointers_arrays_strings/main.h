e8c5ac13fdc14ab02ca0f51a1cd909df0623cea0
Maqa001 <mma.malikov@mail.ru>
2024-03-03 18:45:04 +0000

    l

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -13,3 +13,11 @@ int _putchar(char n);
 int _putchar(char c);
 void rev_string(char *s);
 void swap_char(char *a, char *b);
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
+void reverse_array(int *a, int n);
+char *string_toupper(char *);
+char *cap_string(char *);
+
