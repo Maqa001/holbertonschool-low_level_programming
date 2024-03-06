6b418b37953546b6cb3193d84f84b9c011542e1c
Maqa001 <mma.malikov@mail.ru>
2024-03-06 07:45:37 +0000

    l

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,3 +1,6 @@
+unsigned int _strspn(char *s, char *accept);
+char *_strchr(char *s, char c);
+char *_memcpy(char *dest, char *src, unsigned int n);
 int _strlen(char *s);
 void swap_int(int *a, int *b);
 void reset_to_98(int *n);
@@ -20,4 +23,8 @@ int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
 char *string_toupper(char *);
 char *cap_string(char *);
-
+char *_memset(char *s, char b, unsigned int n);
+char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
+void print_chessboard(char (*a)[8]);
+void print_diagsums(int *a, int size);
