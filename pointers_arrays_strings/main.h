57332238169d2aa9d868c9967d3db84da58d84ee
Maqa001 <mma.malikov@mail.ru>
2024-03-06 07:53:47 +0000

    l

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -30,3 +30,4 @@ void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
+unsigned int _strspn(char *s, char *accept);
